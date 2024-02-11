ad67c43f5cb28b312667ba65203ba1299aaf8913
wrg2674 <wrg267499@naver.com>
2024-02-12 00:35:04 +0900

    Texture Exercises 1~2

---
+++ Texture/shader/texture1.fs
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main(){
+	vec2 reverseTexCoord = vec2(1-TexCoord.x, TexCoord.y);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, reverseTexCoord), 0.2);
+}
